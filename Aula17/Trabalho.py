cbc9b4f3d675b85476d3b1c9807b9391b705eb55
PabloSchumacher <pabloschumacherropelato@gmail.com>
2019-12-12 11:32:31 -0300

    Resolvendo "Trabalho" da Aula17

--- Aula17/Trabalho.py
+++ Aula17/Trabalho.py
@@ -12,14 +12,14 @@ menu = ('''
 7 - Sair
 Informe qual menu você qual opção você deseja: ''')    
 
-def dados(iduser, nome, idade, cpf, telefone, pais, estado, cidade, rua, n):
+def dadosclientes(iduser, nome, idade, cpf, telefone, pais, estado, cidade, rua, n):
     dados = {'id':iduser, 'nome':nome, 'idade':idade, 'cpf':cpf, 'telefone':telefone, 'pais':pais, 'estado':estado, 'cidade':cidade, 'rua':rua, 'n':n}
     return dados
 
 listacadastro = []
-def cadastro():
+def cadastroclientes():
     cadastro = open('Aula17/cadastro.txt','a')
-    cadastro.write(f'{dados["id"]};{dados["nome"]};{dados["idade"]};{dados["cpf"]};{dados["telefone"]};{dados["pais"]};{dados["estado"]};{dados["cidade"]};{dados["rua"]};{dados["n"]}\n')
+    cadastro.write(f'ID: {dados["id"]};Nome: {dados["nome"]};Idade: {dados["idade"]};CPF: {dados["cpf"]};Telefone: {dados["telefone"]};País: {dados["pais"]};Estado: {dados["estado"]};Cidade: {dados["cidade"]};Rua: {dados["rua"]};Número: {dados["n"]}\n')
     cadastro.close()
     return cadastro
 
@@ -30,7 +30,7 @@ def dadosprodutos(idprod, nome, marca, validade, garantia):
 listacadastroprodutos = []
 def cadastroprodutos():
     cadastroprodutos = open('Aula17/cadastroprodutos.txt','a')
-    cadastroprodutos.write(f'{dadosprodutos["id"]};{dadosprodutos["nome"]};{dadosprodutos["marca"]};{dadosprodutos["validade"]};{dadosprodutos["garantia"]}\n')
+    cadastroprodutos.write(f'ID: {dadosprodutos["id"]};Nome: {dadosprodutos["nome"]};Marca: {dadosprodutos["marca"]};Validade: {dadosprodutos["validade"]};Garantia: {dadosprodutos["garantia"]}\n')
     cadastroprodutos.close()
     return cadastroprodutos
 
@@ -40,7 +40,7 @@ def ler_dados():
     for linha in cadastro:
         linha = linha.strip()
         dados_dados = linha.split(';')
-        dadoss = dados(dados_dados[0],dados_dados[1],dados_dados[2],dados_dados[3],dados_dados[4],dados_dados[5],dados_dados[6],dados_dados[7],dados_dados[8],dados_dados[9])
+        dadoss = dadosclientes(dados_dados[0],dados_dados[1],dados_dados[2],dados_dados[3],dados_dados[4],dados_dados[5],dados_dados[6],dados_dados[7],dados_dados[8],dados_dados[9])
         lista_dados.append(dadoss)
     cadastro.close()
     return lista_dados
@@ -57,28 +57,31 @@ def ler_dadosprodutos():
     cadastroprodutos.close()
     return listacadastroprodutos
 
+lista_de_vendas = []
+ler_dados()
+ler_dadosprodutos()
 while True:
     opcao = input(menu)
 
     if opcao == '1':
-        iduser = input('Informe o ID: ')
+        iduser = int(input('Informe o ID: '))
         nome = input('Informe o nome: ')
-        idade = input('Informe a idade: ')
+        idade = int(input('Informe a idade: '))
         cpf = input('Informe o CPF: ')
         telefone = input('Informe o telefone para contato: ')
         pais = input('Informe o país: ')
         estado = input('Informe o estado: ')
         cidade = input('Informe a cidade: ')
         rua = input('Informe a rua: ')
-        n = input('Informe o número da casa: ')
-        dados = dados(iduser, nome, idade, cpf, telefone, pais, estado, cidade, rua, n)
-        cadastro = cadastro()
+        n = int(input('Informe o número da casa: '))
+        dados = dadosclientes(iduser, nome, idade, cpf, telefone, pais, estado, cidade, rua, n)
+        cadastro = cadastroclientes()
 
     elif opcao == '2':
         print(ler_dados())
 
     elif opcao == '3':
-        idprod = input('Informe o ID: ')
+        idprod = int(input('Informe o ID: '))
         nome = input('Informe o nome: ')
         marca = input('Informe a marca: ')
         validade = input('Informe a validade: ')
@@ -90,10 +93,30 @@ while True:
         print(ler_dadosprodutos())
 
     elif opcao == '5':
-        print('Vendas')
+        vendas = open('Aula17/vendas.txt','a')
+        cliente = input('Informe o ID do cliente: ')
+        produto = input('Informe o ID do produto: ')
+        dicionarioclipro = {'iduser':cliente,'idprod':produto}
+        lista_de_vendas.append(dicionarioclipro)
+        vendas.write(f'ID Cliente: {cliente},ID Produto: {produto}\n')
+        vendas.close()
+        print(f'Venda registrada para cliente com ID: {cliente} e produto com iD: {produto}')
 
     elif opcao == '6':
-        print('Relatório de Vendas')
+        # print('Relatório de Vendas')
+        vendas = open('Aula17/vendas.txt','r')
+        cadastroprodutos = open('Aula17/cadastroprodutos.txt','r')
+        cadastroclientes = open('Aula17/cadastro.txt','r')
+        relatoriodevendas = open('Aula17/relatoriodevendas.txt','a')
+        # codigo = input('das')
+        # for dado in dadosprodutos:
+        #   if pessoa['idprod'] == codigo:
+
+
+         
+        vendas.close()
+        cadastroprodutos.close()
+        cadastroclientes.close()
 
     elif opcao == '7':
         print('Sair')
