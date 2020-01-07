da6c4ff14877f321b4984aeddca926a4ff43ac7c
PabloSchumacher <pabloschumacherropelato@gmail.com>
2020-01-07 09:53:33 -0300

    Ex Aula 29 Pronto

--- Aula29/dupla.py
+++ Aula29/dupla.py
@@ -1,49 +1,66 @@
 terminal = ['Presidiário','Policial' , 'Chefe', 'Oficial1', 'Oficial2','Piloto', 'Comissária1','Comissária2']
 aviao = []
-carro = []
 
-def mostrar(terminal,aviao,carro):
-    while terminal.count != 0:
-        a = input('Precione ENTER para continuar')
-        
-        if a == '':
-            if terminal[0] == 'Piloto' and terminal[len(terminal)-1] != 'Piloto':
-                pas = terminal.pop(1)
-                aviao.append(pas)
+def transporte():
+    if not terminal:
+        return(f'Todos foram transportados')
+    elif terminal[0] == 'Presidiário' or terminal[0] == 'Policial':
+        return(f'O {terminal[0]} está sendo transportado e o {terminal[1]} está dirigindo.')
+    elif terminal[0] == 'Chefe' and terminal[1] != 'Piloto':
+        return(f'O {terminal[1]} está sendo transportado e o {terminal[0]} está dirigindo.')
+    elif terminal[0] == 'Chefe':
+        return(f'O {terminal[0]} está sendo transportado e o {terminal[1]} está dirigindo.')
+    elif terminal[0] == 'Piloto' and terminal[len(terminal)-1] != 'Piloto.':
+        return(f'A {terminal[1]} está sendo transportado e o {terminal[0]} está dirigindo.')
 
-                print (f'''
+
+def menu():
+    print (f'''
                 ###############################################################
-
-                Passageiros para Embarque: {terminal}  
-                Passageiros no Carro: {carro}
+                
+                Passageiros para Embarque: {terminal}
+                {transporte()}
                 Passageiros Embarcados no Avião: {aviao}                              
                         
-                ###############################################################''')
+                ###############################################################\n''')
+                
 
-            elif terminal[0] == 'Chefe' and terminal[1] != 'Piloto':
-                pas = terminal.pop(1)
-                aviao.append(pas)
+menu()
 
-                print (f'''
-                ###############################################################
+def mostrar(terminal,aviao):
+    try: 
+        while terminal.count != 0:
+            a = input('\nPrecione ENTER para continuar')
+            
+            if a == '':
 
-                Passageiros para Embarque: {terminal}  
-                Passageiros no Carro: {carro}
-                Passageiros Embarcados no Avião: {aviao}                              
-                        
-                ###############################################################''')
+                if terminal[0] == 'Piloto' and terminal[len(terminal)-1] != 'Piloto' and terminal[1] == 'Comissária2':
+                    pas = terminal.pop(1)
+                    aviao.append(pas)
+                    pas = terminal.pop(0)
+                    aviao.append(pas)
 
-            else:
-                pas = terminal.pop(0)
-                aviao.append(pas)
+                    menu()
+                    
+                elif terminal[0] == 'Piloto' and terminal[len(terminal)-1] != 'Piloto':
+                    pas = terminal.pop(1)
+                    aviao.append(pas)
 
-                print (f'''
-                ###############################################################
+                    menu()
 
-                Passageiros para Embarque: {terminal}  
-                Passageiros no Carro: {carro}
-                Passageiros Embarcados no Avião: {aviao}                              
-                        
-                ###############################################################''')
-    
-mostrar(terminal,aviao,carro)
+                elif terminal[0] == 'Chefe' and terminal[1] != 'Piloto':
+                    pas = terminal.pop(1)
+                    aviao.append(pas)
+
+                    menu()
+
+                else:
+                    pas = terminal.pop(0)
+                    aviao.append(pas)
+
+                    menu()
+    except IndexError as erro:
+        print('Erro de retirar um elemento que não existe: ', erro)
+        print('Todos estão no avião.')
+
+mostrar(terminal,aviao)
