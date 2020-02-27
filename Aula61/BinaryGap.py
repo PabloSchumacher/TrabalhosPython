75be4cf382086d36337e76a8ebd97dac26735f39
PabloSchumacher <pabloschumacherropelato@gmail.com>
2020-02-27 10:59:00 -0300

    Corrigindo Ex2

--- Aula61/BinaryGap.py
+++ Aula61/BinaryGap.py
@@ -4,17 +4,17 @@ def solucao(n):
     binario = bin(n)
     binario = binario.replace('0b','')
     print(f'O seu número binário é: {binario}')
+    valida_gap = False
     maior_gap = 0
     gap = 0
     for i in binario:
         if i == '1':
             if gap > maior_gap:
                 maior_gap = gap
-            x = binario.index(i) #O problema ta nessa linha
             gap = 0
-            while binario[x] == '0':
+            valida_gap = True
+        elif i == '0' and valida_gap == True:
                 gap += 1
-                x += 1
 
     print(f'O maior gap é de: {maior_gap}')
 
